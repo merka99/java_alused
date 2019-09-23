d410c21c19ece09576437985a34afbc5a77cb62d
Merle Päkko <merlepakko@gmail.com>
2019-09-23 09:38:47 +0300

    Esimene

---
+++ java/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Merle Päkko");
+        System.out.println("Hello World!");
+        System.out.print(" (and all the people of the world.)");
+        System.out.println(             );
+    }
+}
