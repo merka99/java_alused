10c071bcd6dd5cfe561c3d35ebafeb8eb7872a5d
Merle Päkko <merlepakko@gmail.com>
2019-10-14 13:14:15 +0300

    2 week

--- java/src/Exercises4.java
+++ java/src/Exercises4.java
@@ -3,6 +3,71 @@ import java.util.Scanner;
 public class Exercises4 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+
+
+      /*  System.out.println("Sisesta arv: ");
+        int i = Integer.parseInt(reader.nextLine());
+
+        int sum = 0;
+        int num = 1;
+        while (num <= i) {
+            sum += num;  // this is the same as result = result + 3;
+            num++;          // means the same as i = i+1;
+        }
+        System.out.println("Summa on:" + sum);
+*/
+
+       /* // ex 31
+        System.out.println("Sisesta arv kust numbririda algab: ");
+        int number = Integer.parseInt(reader.nextLine());
+        System.out.println("Sisesta arv kuhuni numbririda jookseb: ");
+        int number1 = Integer.parseInt(reader.nextLine());
+        while (number <= number1 ) {
+            System.out.println(number);
+            number++;
+        }*/
+
+        //ex 30
+       /* int number1 = 1;
+        System.out.println("Sisesta arv kuhuni numbririda jookseb: ");
+        int number = Integer.parseInt(reader.nextLine());
+        while (number1 <= number ) {
+            System.out.println(number1);
+            number1++;
+        }
+*/
+        // ex 29
+      /*  int number = 2;
+
+        while (number < 101 ) {
+            System.out.println(number);
+            number = number + 2 ;
+        }*/
+
+
+       /* //ex 28
+        int number = 100;
+
+        while (number >= 1 ) {
+            System.out.println(number);
+            number-- ;
+        }*/
+
+
+
+       /*// ex 27
+        int number = 1;
+
+        while (number < 101 ) {
+            System.out.println(number);
+            number ++ ;
+        }
+*/
+
+        // ex 26
+/*
+
         System.out.println("0 sisestades enam numbreid ei küsita: ");
         int sum = 0;
         while (true) {
@@ -15,6 +80,7 @@ public class Exercises4 {
         }
 
         System.out.println("Sum in the end: " + sum);
+*/
 
 
       /*  //ex 25
